d6a2d27203460eb68f75699a1dd0d9cf4fe7591d
Fulop Alex-Dominic <Alex.Fulop@swisscom.com>
2022-10-20 21:26:04 +0300

    added initial state support

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,19 +7,6 @@ const PATTERNS = [
   [0, 2, 3, 5, 6, 8],
 ];
 
-const heading = document.getElementById("title");
-
-let playerOneThrow = Math.floor(Math.random() * 6);
-let playerTwoThrow = Math.floor(Math.random() * 6);
-
-if (playerOneThrow > playerTwoThrow) {
-  heading.innerText = "🚩Player one wins!";
-} else if (playerTwoThrow > playerOneThrow) {
-  heading.innerText = "Player two wins!🚩";
-} else {
-  heading.innerText = "Draw!";
-}
-
 function renderDice(selector, playerThrow) {
   let diceDots = document.querySelectorAll(selector + " .dot");
   let dicePattern = PATTERNS[playerThrow];
@@ -28,5 +15,26 @@ function renderDice(selector, playerThrow) {
     diceDots[dicePattern[i]].classList.add("visible");
 }
 
-renderDice("#player-one-dice", playerOneThrow);
-renderDice("#player-two-dice", playerTwoThrow);
+var reloading = sessionStorage.getItem("reloading");
+
+if (reloading) {
+  const heading = document.getElementById("title");
+
+  let playerOneThrow = Math.floor(Math.random() * 6);
+  let playerTwoThrow = Math.floor(Math.random() * 6);
+
+  if (playerOneThrow > playerTwoThrow) {
+    heading.innerText = "🚩Player one wins!";
+  } else if (playerTwoThrow > playerOneThrow) {
+    heading.innerText = "Player two wins!🚩";
+  } else {
+    heading.innerText = "Draw!";
+  }
+
+  renderDice("#player-one-dice", playerOneThrow);
+  renderDice("#player-two-dice", playerTwoThrow);
+} else {
+  sessionStorage.setItem("reloading", true);
+  renderDice("#player-one-dice", 5);
+  renderDice("#player-two-dice", 5);
+}
